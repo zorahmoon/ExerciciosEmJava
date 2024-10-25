426515ee84fdc4890e85e1356ef5644ab006139d
Ester Miranda Cobos <ester.miranda.cobos@gmail.com>
2024-10-25 15:21:53 -0300

    décimo segundo commit

--- exercicio12.java
+++ exercicio12.java
@@ -1,16 +1,20 @@
-public import java.util.Scanner;
+import java.util.Scanner;
 
 public class exercicio12 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite uma string para inverter: ");
-        String stringUsuario = scanner.nextLine();
+        String input = scanner.nextLine();
 
-        String stringInvertida = new StringBuilder(stringUsuario).reverse().toString();
+        String inverted = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            inverted += input.charAt(i); 
+        }
 
-        System.out.println("String invertida: " + stringInvertida);
+        System.out.println("String invertida: " + inverted);
 
         scanner.close();
     }
-} 
+}
