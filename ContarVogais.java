6929d771c2cc897ee7753037a3c333fc173052fe
Ester Miranda Cobos <ester.miranda.cobos@gmail.com>
2024-10-25 16:45:21 -0300

    décimo sétimo commit

---
+++ ContarVogais.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ContarVogais {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma frase: ");
+        String frase = scanner.nextLine();
+
+        int contadorVogais = 0;
+
+        frase = frase.toLowerCase();
+
+        for (int i = 0; i < frase.length(); i++) {
+            char letra = frase.charAt(i); 
+
+            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+                contadorVogais++; 
+            }
+        }
+
+        System.out.println("Número de vogais na frase: " + contadorVogais);
+
+        scanner.close();
+    }
+}
